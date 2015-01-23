00abc1df97742547220353ace214841c16488cd8
BazZy <bazzy.bazzy@gmail.com>
2015-01-23 12:54:06 +0300

    footer responsive fixes

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
 
-connect().use(serveStatic(__dirname + "/build")).listen(8080);
+connect().use(serveStatic(__dirname + "/build")).listen(8080, "0.0.0.0");
